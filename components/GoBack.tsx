9ccb50bb1242970d9f38c70cdee361014ffde1cc
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-10-28 21:04:43 +0530

    search

--- components/GoBack.tsx
+++ components/GoBack.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 const Goback = () => {
   return (
-    <div className="py-2 px-6 fixed top-0 left-0">
+    <div className=" p-6 fixed top-0 left-0 w-full">
       <Link href={"/"}>
         <img src="/icons/left.svg" alt="" />
       </Link>
+      {/* <div className="text-center text-[22px] py-2 font-bold  w-full ">
+        Auditoriums
+      </div> */}
     </div>
   );
 };
---
+++ pages/search.tsx
@@ -0,0 +1,12 @@
+import Goback from "@/components/GoBack";
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className="font-Montserrat">
+      <Goback />
+    </div>
+  );
+};
+
+export default Search;
